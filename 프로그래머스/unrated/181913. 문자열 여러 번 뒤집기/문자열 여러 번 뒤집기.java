fc445c3036ae18930cfccfae460235c7e632a620
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2023-09-04 16:35:58 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 11.38 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -5,15 +5,12 @@ class Solution {
         String answer = "";
         String[] sArr = my_string.split("");
         for(int i = 0; i < queries.length; i++) {
-            String temp = "";
             int left = queries[i][0];
             int right = queries[i][1];
             while(left < right) {
-                temp = sArr[right];
-                sArr[right] = sArr[left];
-                sArr[left] = temp;
-                left++;
-                right--;
+                String temp = sArr[right];
+                sArr[right--] = sArr[left];
+                sArr[left++] = temp;
             }
         }
         for(int i = 0; i < sArr.length; i++) {
