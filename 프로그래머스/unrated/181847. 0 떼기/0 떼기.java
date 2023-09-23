e8604b4aa6e501c71143eb334bf70a0c1e1b1373
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2023-09-23 22:21:10 +0900

    [unrated] Title: 0 떼기, Time: 7.51 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,18 +1,7 @@
 class Solution {
     public String solution(String n_str) {
         String answer = "";
-        for(int i = 0; i < n_str.length(); i++) {
-            if(n_str.charAt(0) != '0') {
-                answer += n_str.charAt(i);
-            } else {
-                if(n_str.charAt(i) != '0') {
-                    for(int j = i; j < n_str.length(); j++){
-                        answer += n_str.charAt(j);
-                    }
-                    break;
-                }
-            }
-        }
+        answer = Integer.parseInt(n_str) + "";
         return answer;
     }
 }
