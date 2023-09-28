6f93908fbdb457f049a508b727636df4a54ea5f5
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2023-09-28 22:36:09 +0900

    [unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
         int answer = 0;
-        if(arr1.length > arr2.length) {
-            answer = 1;
-        } else if(arr1.length < arr2.length) {
-            answer = -1;
-        } else {
+        if(arr1.length > arr2.length) answer = 1;
+        else if(arr1.length < arr2.length) answer = -1;
+        else {
             int sum1 = 0;
             int sum2 = 0;
             for(int i = 0; i < arr1.length; i++) {
@@ -14,13 +12,9 @@ class Solution {
             for(int i = 0; i < arr2.length; i++) {
                 sum2 += arr2[i];
             }
-            if(sum1 > sum2) {
-                answer = 1;
-            } else if(sum1 < sum2) {
-                answer = -1;
-            } else {
-                answer = 0;
-            }
+            if(sum1 > sum2) answer = 1;
+            else if(sum1 < sum2) answer = -1;
+            else answer = 0;
         }
         return answer;
     }
