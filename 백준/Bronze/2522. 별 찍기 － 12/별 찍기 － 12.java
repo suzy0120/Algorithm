f149854c06c133a3338722e4c18abe49c986d9e6
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2024-01-21 22:53:30 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 380 ms, Memory: 18200 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i = 1; i <= 2*N-1; i++) {
+			if(i <= N) {
+				for(int j = 1; j <= N-i; j++) {
+					System.out.print(" ");
+				}
+				for(int j = 1; j <= i; j++) {
+					System.out.print("*");
+				}
+			} else {
+				for(int j = 1; j <= i-N; j++) {
+					System.out.print(" ");
+				}
+				for(int j = i-N; j < N; j++) {
+					System.out.print("*");
+				}
+			}
+			
+			System.out.println();
+		}
+	}
+}
