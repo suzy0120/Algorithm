6109d8d43af46dbeaa195c6ea8325336ffa5952b
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2024-03-06 16:12:26 +0900

    [Silver IV] Title: ATM, Time: 156 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,26 +2,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] num = new int[N];
-		for(int n = 0; n < N; n++) {
-			num[n] = sc.nextInt();
-		}
-		
-		Arrays.sort(num);
-		
-		int[] sum = new int[N];
-		sum[0] = num[0];
-		for(int i = 1; i < N; i++) {
-			sum[i] = sum[i-1] + num[i];
-		}
-		
-		int min = 0;
-		for(int i = 0; i < N; i++) {
-			min += sum[i];
-		}
-		System.out.println(min);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] P = new int[N];
+        for(int i = 0; i < N; i++) {
+            P[i] = sc.nextInt();
+        }
+
+        Arrays.sort(P);
+
+        int[] sum = new int[N];
+        sum[0] = P[0];
+        int min = P[0];
+        for(int i = 1; i < N; i++) {
+            sum[i] = sum[i-1] + P[i];
+            min += sum[i];
+        }
+
+        System.out.println(min);
+    }
 }
