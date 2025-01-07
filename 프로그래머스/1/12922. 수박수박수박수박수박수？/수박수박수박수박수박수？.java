a8913fdde84d4787e1c6d4d6c24a1c84a121059d
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-01-07 15:53:15 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 46.97 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,9 +2,8 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         
-        for(int i=1; i<=n; i++) {
-            char c = i%2==1 ? '수' : '박';
-            answer +=c;
+        for(int i=0; i<n; i++) {
+            answer += (i%2==0) ? "수" : "박";
         }
         
         return answer;
