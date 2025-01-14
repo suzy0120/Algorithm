fcd43364cd5d156a915784623ee0e87329ac6b75
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-01-14 20:18:21 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 24.83 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,12 +2,18 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, (s1, s2) -> {
-            if(s1.charAt(n) == s2.charAt(n))
-                return s1.compareTo(s2);
-            return Character.compare(s1.charAt(n), s2.charAt(n));
-        });
-
-        return strings;
+        String[] answer = new String[strings.length];
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(int i=0; i<strings.length; i++) {
+            list.add("" + strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(list);
+        
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i).substring(1, list.get(i).length());
+        }
+        
+        return answer;
     }
 }
