ab7c48125c6365ac511055bd508801979b831618
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-01-15 13:05:12 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n>=a) {
+            int exchanged = (n/a) * b;
+            answer += exchanged;
+            n = (n%a) + exchanged;
+        }
+        
+        return answer;
+    }
+}
