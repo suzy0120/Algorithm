999d8947d41b354bb43f887616c6b4f4374feb08
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-01-29 22:46:59 +0900

    [level 1] Title: 과일 장수, Time: 60.48 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,18 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int num = score.length;
         Arrays.sort(score);
         
-        while(num >= m) {
-            int min = Integer.MAX_VALUE;
-            
-            for(int i=num-1; i>=num-m; i--) {
-                if(min > score[i]) min = score[i];
-            }
-            
-            answer += min*m;
-            num -= m;
+        for(int i=score.length-m; i>=0; i-=m) {
+            answer += score[i]*m;
         }
         
         return answer;
