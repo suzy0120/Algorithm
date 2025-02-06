87fde70c9b42e69d40ad357d3072e98bafc7a8ef
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-02-06 23:53:55 +0900

    [level 1] Title: 대충 만든 자판, Time: 8.33 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,34 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
+        Map<Character, Integer> map = new HashMap<>();
         
-        char[][] array = new char[keymap.length][];
-        for(int i=0; i<keymap.length; i++) {
-            array[i] = keymap[i].toCharArray();
+        for(String key : keymap) {
+            for(int i=0; i<key.length(); i++) {
+                char c = key.charAt(i);
+                int count = i+1;
+                if(!map.containsKey(c) || map.get(c) > count) map.put(c, count);
+            }
         }
         
-        int idx = 0;
-        for(String target : targets) {
-            int count = 0;
-            boolean possible = true;
-            for(char t : target.toCharArray()) {
-                int min = Integer.MAX_VALUE;
-                for(int r=0; r<array.length; r++) {
-                    for(int c=0; c<array[r].length; c++) {
-                        if(t==array[r][c]) {
-                            min = Math.min(min, c+1);
-                            break;
-                        }
-                    }
-                }
-                if(min==Integer.MAX_VALUE) {
-                    possible = false;
+        for(int i=0; i<targets.length; i++) {
+            int sum = 0;
+            for(char c : targets[i].toCharArray()) {
+                if(map.containsKey(c)) sum += map.get(c);
+                else {
+                    sum = -1;
                     break;
                 }
-                else count+=min;
             }
-            answer[idx] = possible ? count : -1;
-            idx++;
+            answer[i] = sum;
         }
         
         return answer;
