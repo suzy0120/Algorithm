69b5e5b0706317eed4cad834127c05b93885f190
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-02-15 23:37:28 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.49 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 동영상 재생기 - 340213 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 1.19 ms
+메모리: 86.6 MB, 시간: 1.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 15일 16:21:06
+2025년 02월 15일 23:37:25
 
 ### 문제 설명
 
--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,34 +1,36 @@
-import java.util.*;
-
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int totalVideo = toMinutes(video_len);
-        int current = toMinutes(pos);
-        int opStart = toMinutes(op_start);
-        int opEnd = toMinutes(op_end);
+        int totalVideo = toMinutes(video_len); // 동영상의 길이
+        int current = toMinutes(pos); // 재생위치
+        int start = toMinutes(op_start); // 오프닝 시작 시각
+        int end = toMinutes(op_end); // 오프닝이 끝나는 시각
         
         for(String cmd : commands) {
-            if(current >= opStart && current <= opEnd) current = opEnd;
+            if(current >= start && current <= end) current = end;
             
             if(cmd.equals("next")) {
-                current = Math.min(current + 10, totalVideo);
-                if(current >= opStart && current <= opEnd) current = opEnd;
+                current = Math.min(current+10, totalVideo);
+                
+                if(current >= start && current <= end) current = end;
             } else if(cmd.equals("prev")) {
-                current = Math.max(current - 10, 0);
-                if(current >= opStart && current <= opEnd) current = opEnd;
+                current = Math.max(current-10, 0);
+                
+                if(current >= start && current <= end) current = end;
             }
         }
         
-        int hh = current / 60;
-        int mm = current % 60;
-        return String.format("%02d:%02d", hh, mm);
+        int hour = current/60;
+        int minute = current%60;
+        
+        String answer = String.format("%02d:%02d", hour, minute);
+        
+        return answer;
     }
     
-    // "HH:MM" 문자열을 분 단위 정수로 변환하는 메서드
-    private int toMinutes(String time) {
+    public int toMinutes(String time) {
         String[] parts = time.split(":");
         int hh = Integer.parseInt(parts[0]);
         int mm = Integer.parseInt(parts[1]);
-        return hh * 60 + mm;
+        return hh*60 + mm;
     }
 }
