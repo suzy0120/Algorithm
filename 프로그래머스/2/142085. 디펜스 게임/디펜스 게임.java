c25a655161c49016af9f55bcaed1531bfe31800e
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-02-24 23:59:36 +0900

    [level 2] Title: 디펜스 게임, Time: 128.92 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int answer = enemy.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        for(int i = 0; i < enemy.length; i++) {
-            pq.add(enemy[i]);
+        for(int i=0; i<enemy.length; i++) {
+            pq.offer(enemy[i]);
             n -= enemy[i];
+            
             if(n < 0) {
-                if(k <= 0) {
+                if(k < 1) {
                     answer = i;
                     break;
                 } else {
