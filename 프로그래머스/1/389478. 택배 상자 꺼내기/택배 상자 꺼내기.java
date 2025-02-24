f9f87db25ee3fcaa92b557bedd19c37ea42c0643
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-02-24 19:33:08 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -7,17 +7,17 @@ class Solution {
         int pr = num%w;
         
         if(pr == 0) { // w의 배수
-            pq-=1;
-            pr=w;
+            pq--;
+            pr = w;
         }
         
-        if((nq%2==1 && pq%2==1) || (nq%2==0 && pq%2==0)) {
+        if(nq%2 == pq%2) {
             if(nr >= pr) answer = nq - pq + 1;
             else answer = nq - pq;
         } else if(nq%2==1 && pq%2==0) {
             if(w-nr > pr-1) answer = nq - pq;
             else answer = nq - pq + 1;
-        } else if((nq%2==0 && pq%2==1)) {
+        } else {
             if(nr-1 >= w-pr) answer = nq - pq + 1;
             else answer = nq - pq;
         }
