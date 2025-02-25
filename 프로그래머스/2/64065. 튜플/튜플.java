91d2070940898e824f48edd79c40652e55075528
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-02-25 22:38:56 +0900

    [level 2] Title: 튜플, Time: 93.78 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,35 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        s = s.replaceAll("[{},]", " ");
-        String[] arr = s.split(" ", -1);
+        String[] arr = s.split("[{},]+");
         
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i].equals("")) continue;
-            
-            int num = Integer.parseInt(arr[i]);
-            map.put(num, map.getOrDefault(num, 0)+1);
-        }
-        
-        int[][] kv = new int[map.size()][2];
-        int idx = 0;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            kv[idx][0] = entry.getKey();
-            kv[idx][1] = entry.getValue();
-            idx++;
-        }
-        
-        Arrays.sort(kv, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return Integer.compare(b[1], a[1]);
+        for(String str : arr) {
+            if(!str.isEmpty()) {
+                int num = Integer.parseInt(str);
+                map.put(num, map.getOrDefault(num, 0)+1);
             }
-        });
+        }
         
-        int[] answer = new int[map.size()];
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort((a, b) -> b.getValue() - a.getValue());
+        
+        int[] answer = new int[entryList.size()];
         for(int i=0; i<answer.length; i++) {
-            answer[i] = kv[i][0];
+            answer[i] = entryList.get(i).getKey();
         }
         
         return answer;
