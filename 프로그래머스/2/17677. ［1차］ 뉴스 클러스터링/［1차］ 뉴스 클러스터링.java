4a256ecc901f4d9e976d3067d9e4ad3d6b2b19d0
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-03-01 01:01:45 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.43 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -5,39 +5,42 @@ class Solution {
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
         
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
         
-        int count1 = 0;
         for(int i=0; i<str1.length()-1; i++) {
             if(Character.isLetter(str1.charAt(i)) && Character.isLetter(str1.charAt(i+1))) {
                 String s = str1.substring(i, i+2);
                 
-                map.put(s, map.getOrDefault(s, 0)+1);
-                
-                count1++;
+                map1.put(s, map1.getOrDefault(s, 0)+1);
             }
         }
         
-        int same = 0;
-        int count2 = 0;
         for(int i=0; i<str2.length()-1; i++) {
             if(Character.isLetter(str2.charAt(i)) && Character.isLetter(str2.charAt(i+1))) {
                 String s = str2.substring(i, i+2);
                 
-                if(map.containsKey(s) && map.get(s) > 0) {
-                    map.replace(s, map.get(s)-1);
-                    same++;
-                }
-                
-                count2++;
+                map2.put(s, map2.getOrDefault(s, 0)+1);
             }
         }
         
-        int sum = count1 + count2 - same;
+        Set<String> keys = new HashSet<>();
+        keys.addAll(map1.keySet());
+        keys.addAll(map2.keySet());
+        
+        int same = 0;
+        int sum = 0;
+        for(String key : keys) {
+            int count1 = map1.getOrDefault(key, 0);
+            int count2 = map2.getOrDefault(key, 0);
+            
+            same += Math.min(count1, count2);
+            sum += Math.max(count1, count2);
+        }
+        
         if(sum == 0) return 65536;
         
         int answer = same * 65536 / sum;
-        
         return answer;
     }
 }
