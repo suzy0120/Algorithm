bfd04b35ac02f417b25592501f7a7cb8d5ad5959
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-03-03 18:16:55 +0900

    [level 2] Title: [3차] 압축, Time: 27.33 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,38 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        Map<String, Integer> dictionary = new LinkedHashMap<>();
-        char c = 'A';
-        for(int i=1; i<=26; i++) {
-            String s = String.valueOf(c);
-            dictionary.put(s, i);
-            c++;
+        Map<String, Integer> dictionary = new HashMap<>();
+        List<Integer> output = new ArrayList<>();
+        
+        for(int i=0; i<26; i++) {
+            dictionary.put(String.valueOf((char) ('A'+i)), i+1);
         }
         
-        List<Integer> list = new ArrayList<>();
-        int num = 26;
-        loop : for(int i=0; i<msg.length(); i++) {
-            boolean visited = false;
-            int len = 0;
-            for(int j=i+1; j<=msg.length(); j++) {
-                String s = msg.substring(i, j);
-                if(dictionary.containsKey(s)) {
-                    if(visited) list.remove(list.size()-1);
-                    list.add(dictionary.get(s));
-                    if(i+s.length() >= msg.length()) break loop;
-                    visited = true;
-                    len = s.length()-1;
-                } else {
-                    dictionary.put(s, num+=1);
-                    i+=len;
-                    break;
-                }
+        int num = 27;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<msg.length();) {
+            sb.setLength(0); // StringBuilder 초기화
+            sb.append(msg.charAt(i));
+            int j=i+1;
+            
+            while(j < msg.length() && dictionary.containsKey(sb.toString()+msg.charAt(j))) {
+                sb.append(msg.charAt(j));
+                j++;
             }
+            
+            output.add(dictionary.get(sb.toString()));
+            
+            if(j < msg.length()) dictionary.put(sb.toString()+msg.charAt(j), num++);
+            
+            i += sb.length();
         }
         
-        int[] answer = new int[list.size()];
+        int[] answer = new int[output.size()];
         for(int i=0; i<answer.length; i++) {
-            answer[i] = list.get(i);
+            answer[i] = output.get(i);
         }
         
         return answer;
