beb5c339c84309ca6b56ec20fb3c053c4836ec37
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-03-10 10:27:36 +0900

    [level 2] Title: 오픈채팅방, Time: 158.95 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,38 +2,31 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        int count = 0;
-        for(int i=0; i<record.length; i++) {
-            if(record[i].charAt(0) != 'C') count++;
+        Map<String, String> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        
+        for(String r : record) {
+            String[] word = r.split(" ");
+            
+            if(!word[0].equals("Leave")) map.put(word[1], word[2]);
         }
         
-        Map<String, String> map = new HashMap<>();
-        
-        for(int i=0; i<record.length; i++) {
-            String[] word = record[i].split(" ");
+        for(String r : record) {
+            String[] word = r.split(" ");
             
             if(word[0].equals("Enter")) {
-                map.put(word[1], word[2]);
-            } else if(word[0].equals("Change")) {
-                if(map.containsKey(word[1])) {
-                    map.replace(word[1], word[2]);
-                }
+                list.add(map.get(word[1]) + "님이 들어왔습니다.");
+            } else if(word[0].equals("Leave")) {
+                list.add(map.get(word[1]) + "님이 나갔습니다.");
             }
         }
         
-        String[] answer = new String[count];
-        int idx =0;
-        for(String s : record) {
-            String[] word = s.split(" ");
-            if(word[0].equals("Enter") && map.containsKey(word[1])) {
-                answer[idx] = map.get(word[1]);
-                answer[idx++] += "님이 들어왔습니다.";
-            } else if(word[0].equals("Leave")) {
-                answer[idx] = map.get(word[1]);
-                answer[idx++] += "님이 나갔습니다.";
-            }
+        String[] answer = new String[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;
+        // return list.toArray(new String[0]);
     }
 }
