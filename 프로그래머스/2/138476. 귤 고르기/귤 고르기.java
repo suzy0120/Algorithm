e9fcd31ee564d39415d184272fdc482d0a8499d1
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-03-11 11:43:38 +0900

    [level 2] Title: 귤 고르기, Time: 54.95 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,19 +4,20 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i=0; i<tangerine.length; i++) {
-            if(map.containsKey(tangerine[i])) map.replace(tangerine[i], map.get(tangerine[i])+1);
-            else map.put(tangerine[i], 1);
+        for(int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0)+1);
         }
         
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue().compareTo(a.getValue()));
-        pq.addAll(map.entrySet());
+        List<Integer> list = new ArrayList<>(map.values());
+        list.sort(Collections.reverseOrder());
         
         int count = 0;
         int answer = 0;
-        while(count < k) {
-            count += pq.poll().getValue();
+        for(int n : list) {
+            count += n;
             answer++;
+            
+            if(count >= k) break;
         }
         
         return answer;
