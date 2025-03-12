9f8d5ea57ba959c31fafffc9bac914579f88d11b
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-03-12 22:40:51 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 12.68 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -2,24 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, String[] board) {
-        int answer = 0;
-        
-        char[][] cBoard = new char[m][n];
+        char[][] game = new char[m][n];
         for(int r=0; r<m; r++) {
-            cBoard[r] = board[r].toCharArray();
+            game[r] = board[r].toCharArray();
         }
         
+        int answer = 0;
         while(true) {
             boolean[][] visited = new boolean[m][n];
             boolean found = false;
             
             for(int r=0; r<m-1; r++) {
                 for(int c=0; c<n-1; c++) {
-                    char base = cBoard[r][c];
-                    
-                    if(base != 'x' && base == cBoard[r+1][c] &&
-                       base == cBoard[r][c+1] && base == cBoard[r+1][c+1]) {
-                        visited[r][c] = visited[r+1][c] = visited[r][c+1] = visited[r+1][c+1] = true;
+                    if(game[r][c] != 'x' && game[r][c] == game[r][c+1] && game[r][c] == game[r+1][c] && game[r][c] == game[r+1][c+1]) {
+                        visited[r][c] = visited[r][c+1] = visited[r+1][c] = visited[r+1][c+1] = true;
                         found = true;
                     }
                 }
@@ -30,24 +26,20 @@ class Solution {
             for(int r=0; r<m; r++) {
                 for(int c=0; c<n; c++) {
                     if(visited[r][c]) {
+                        game[r][c] = 'x';
                         answer++;
-                        cBoard[r][c] = 'x';
                     }
                 }
             }
             
             for(int c=0; c<n; c++) {
-                List<Character> list = new ArrayList<>();
-                
+                int row = m-1;
                 for(int r=m-1; r>=0; r--) {
-                    if(cBoard[r][c] != 'x') list.add(cBoard[r][c]);
+                    if(game[r][c] != 'x') game[row--][c] = game[r][c];
                 }
                 
-                int row = m-1;
-                for(char ch : list) cBoard[row--][c] = ch;
-                
                 while(row >= 0) {
-                    cBoard[row--][c] = 'x';
+                    game[row--][c] = 'x';
                 }
             }
         }
