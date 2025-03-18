c67c23814a842544a1b75664ffb133f326c21203
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-03-18 11:57:49 +0900

    [level 0] Title: 문자열 밀기, Time: 12.38 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,14 +1,17 @@
 class Solution {
     public int solution(String A, String B) {
+        int answer = -1;
         int len = A.length();
         
-        if(len != B.length()) return -1;
-        
         for(int i=0; i<len; i++) {
-            if(A.equals(B)) return i;
+            if(A.equals(B)) {
+                answer = i;
+                break;
+            }
+            
             A = A.charAt(len-1) + A.substring(0, len-1);
         }
         
-        return -1;
+        return answer;
     }
 }
