474f741e2932b7542281df0979cf5456c015570f
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-01 23:47:27 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] arr) {
+        int lcm = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            lcm = getLCM(lcm, arr[i]);
+        }
+        
+        return lcm;
+    }
+
+    // 두 수의 최소공배수 구하기
+    private int getLCM(int a, int b) {
+        return a * b / getGCD(a, b);
+    }
+
+    // 두 수의 최대공약수 (유클리드 호제법)
+    private int getGCD(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
