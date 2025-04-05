c1389c621c4b7bc8d01d9b6799ca991fb49ad604
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-05 14:15:50 +0900

    [level 2] Title: 프로세스, Time: 1.62 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for(int i=0; i<priorities.length; i++) {
+            queue.add(new int[] {i, priorities[i]});
+        }
+        
+        int answer = 0;
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            
+            boolean b = true;
+            for(int[] q : queue) {
+                if(q[1] > cur[1]) {
+                    queue.add(cur);
+                    b = false;
+                    break;
+                }
+            }
+            
+            if(b) {
+                answer++;
+                
+                if(cur[0] == location) return answer;
+            }
+        }
+        
+        return -1;
+    }
+}
