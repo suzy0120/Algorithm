650ab47bd9beb22400f31085555257c94836b5d6
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-09 23:15:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 424 ms, Memory: 37136 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static int N; // 정점 수
-    public static int M; // 간선 수
-    public static int V; // 시작할 정점의 번호
     public static int[][] adjArr;
     static boolean[] visited;
     static Queue<Integer> queue = new LinkedList<>();
@@ -13,17 +11,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        M = sc.nextInt();
-        V = sc.nextInt();
-        adjArr = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
+        int M = sc.nextInt(); // 간선 수
+        int V = sc.nextInt(); // 시작할 정점의 번호
         
-        for (int i = 0; i < M; i++) {
+        adjArr = new int[N+1][N+1];
+        for(int i = 0; i < M; i++) {
             int A = sc.nextInt(); // 시작 정점
             int B = sc.nextInt(); // 끝 정점
             adjArr[A][B] = adjArr[B][A] = 1;
         }
 
+        visited = new boolean[N+1];
         DFS(V);
         System.out.println();
         visited = new boolean[N+1];
@@ -36,8 +34,8 @@ public class Main {
     	visited[v] = true;
         System.out.print(v + " ");
         //방문하지 않았으면서, 너와 내가 연결된 간선이 존재한다면 재귀호출
-        for (int i = 0; i < N+1; i++) {
-            if (!visited[i] && adjArr[v][i] == 1)
+        for(int i = 0; i < N+1; i++) {
+            if(!visited[i] && adjArr[v][i] == 1)
             	DFS(i);
         }
     }
@@ -49,13 +47,13 @@ public class Main {
 		visited[v] = true; // 방문처리를 한다.
 		// 큐가 공백이 될때까지 반복문 수행
 		// 큐가 공백이 아니라면 반복문 수행
-		while (!queue.isEmpty()) {
+		while(!queue.isEmpty()) {
 			// 정점을 하나 꺼내자
-			int t = queue.poll();
-			System.out.print(t + " ");
+			int n = queue.poll();
+			System.out.print(n + " ");
 			// 나와 연결되어 있으면서 방문하지 않은 친구들은 Q에 넣고 방문처리를 한다.
-			for (int i = 1; i < adjArr.length; i++) {
-				if (!visited[i] && adjArr[t][i] == 1) {
+			for(int i = 1; i < N+1; i++) {
+				if(!visited[i] && adjArr[n][i] == 1) {
 					queue.add(i); // 큐에 넣고 방문처리
 					visited[i] = true;
 				}
