8d1b624bc172a28ab0a40808ea24f773deeb3d61
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-09 23:09:32 +0900

    [level 2] Title: 모음 사전, Time: 6.52 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -7,7 +7,7 @@ class Solution {
     public int solution(String word) {
         int answer = 0;
         list = new ArrayList<>();
-        dfs("", 0);
+        DFS("", 0);
         int size = list.size();
         for (int i = 0; i < size; i++) {
             if (list.get(i).equals(word)) {
@@ -18,11 +18,11 @@ class Solution {
         return answer;
     }
     
-    static void dfs(String str, int len) {
+    static void DFS(String str, int len) {
         list.add(str);
         if (len == 5) return;
         for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1);
+            DFS(str + words[i], len + 1);
         }
     }
 }
