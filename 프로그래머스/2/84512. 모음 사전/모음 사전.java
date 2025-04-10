b203f7ae0af1df1b6c6fd9f5cf6547dc9b1f9928
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-10 16:38:06 +0900

    [level 2] Title: 모음 사전, Time: 0.22 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,23 +1,22 @@
 import java.util.*;
 
 class Solution {
-    List<String> list = new ArrayList<>();
-    String[] vowel = {"A", "E", "I", "O", "U"};
-    
     public int solution(String word) {
-        DFS("", 0);
+        Map<Character, Integer> first = Map.of('A', 1, 'E', 782, 'I', 1563, 'O', 2344, 'U', 3125);
+        Map<Character, Integer> second = Map.of('A', 1, 'E', 157, 'I', 313, 'O', 469, 'U', 625);
+        Map<Character, Integer> third = Map.of('A', 1, 'E', 32, 'I', 63, 'O', 94, 'U', 125);
+        Map<Character, Integer> fourth = Map.of('A', 1, 'E', 7, 'I', 13, 'O', 19, 'U', 25);
+        Map<Character, Integer> last = Map.of('A', 1, 'E', 2, 'I', 3, 'O', 4, 'U', 5);
         
-        int answer = list.indexOf(word)+1;
-        return answer;
-    }
-    
-    public void DFS(String current, int depth) {
-        if(depth > 5) return;
+        // 맵들을 배열에 담음
+        Map<Character, Integer>[] map = new Map[]{first, second, third, fourth, last};
         
-        if(!current.isEmpty()) list.add(current);
-        
-        for(int i=0; i<vowel.length; i++) {
-            DFS(current+vowel[i], depth+1);
+        int answer = 0;
+        for(int i=0; i<word.length(); i++) {
+            char letter = word.charAt(i);
+            answer += map[i].get(letter);
         }
+        
+        return answer;
     }
 }
