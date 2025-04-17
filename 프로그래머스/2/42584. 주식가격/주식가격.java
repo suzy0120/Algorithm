5f0f55e0e895a963b89a4362e0def1c6d7098dc7
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-17 13:42:40 +0900

    [level 2] Title: 주식가격, Time: 41.07 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            // 가격이 떨어진 지점을 만나면 해당 인덱스들의 정답을 계산
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+        
+        int[] answer = new int[prices.length];
+        for(int i=0; i<prices.length; i++) {
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
                 int idx = stack.pop();
                 answer[idx] = i - idx;
             }
+            
             stack.push(i);
         }
-
-        // 끝까지 가격이 떨어지지 않은 경우 처리
-        while (!stack.isEmpty()) {
+        
+        int n = prices.length;
+        while(!stack.isEmpty()) {
             int idx = stack.pop();
-            answer[idx] = n - 1 - idx;
+            answer[idx] = n-1-idx;
         }
-
+        
         return answer;
     }
 }
