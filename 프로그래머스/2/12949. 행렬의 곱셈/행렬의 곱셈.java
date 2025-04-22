25a50a4434b8ab12e58bf4f891840cd78698a13d
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-22 21:39:25 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.31 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        // m*k * k*n --> m*n
         int[][] answer = new int[arr1.length][arr2[0].length];
-        // System.out.println(answer.length); // 3
-        // System.out.println(answer[0].length); // 2
         
-        // i : answer.length == 3
-        // j : answer[0].length == 2
-        // k : arr2.length == 2
-        for(int i = 0; i < answer.length; i++){
-            for(int j = 0; j < answer[0].length; j++){
-                int sum = 0;
-                for(int k = 0; k < arr2.length; k++){
-                    sum += arr1[i][k] * arr2[k][j];
-                    //System.out.println("i : " + i + "  j : " + j + "  k : " + k + "  sum : " + sum);
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr2[0].length; j++) {
+                for(int k=0; k<arr2.length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
-                answer[i][j] = sum;
             }
         }
+        
         return answer;
     }
 }
