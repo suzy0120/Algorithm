1f81d3dabf42d478d6f1ea00d5b383eaa5753c05
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-28 14:55:40 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,13 +4,13 @@ class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        for(String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0)+1);
         }
         
         int answer = 1;
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            answer *= (entry.getValue()+1);
+        for(int value : map.values()) {
+            answer *= (value+1);
         }
         
         return answer-1;
