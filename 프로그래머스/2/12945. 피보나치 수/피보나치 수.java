30577ba0b3c0a3045b22f809337652456597ea68
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-04-28 11:44:15 +0900

    [level 2] Title: 피보나치 수, Time: 1.65 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n) {
-        if(n==2) return 1;
+        if(n == 2) return 1;
         
         int answer = 0;
         int a=1, b=1;
+        
         for(int i=3; i<=n; i++) {
-            answer = (a+b) % 1234567;
+            answer = (a + b) % 1234567;
             a = b;
             b = answer;
         }
