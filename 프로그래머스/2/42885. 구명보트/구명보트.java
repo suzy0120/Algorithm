52af2a260c8f686a3970da3b242f072cab280d9a
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-05-02 12:07:29 +0900

    [level 2] Title: 구명보트, Time: 11.86 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,10 +4,11 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int i = 0, j = people.length - 1;
+        int i = 0, j = people.length-1;
         
         while(i <= j) {
             if(people[i] + people[j] <= limit) i++;
+            
             j--;
             answer++;
         }
