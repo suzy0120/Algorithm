6ce20f55ba69d7d4aa8c003bbbc2c57238d4ccfe
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-05-10 23:48:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int n, int a, int b) {
         int answer = 0;
-        int pos1 = a;
-        int pos2 = b;
+        int pos1 = a, pos2 = b;
         
         while(pos1 != pos2) {
             pos1 = (pos1/2) + (pos1%2);
