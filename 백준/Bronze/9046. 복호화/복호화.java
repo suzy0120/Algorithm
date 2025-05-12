df7c143ba68ba90cf69e2a47a8144661ef334305
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-05-12 22:14:21 +0900

    [Bronze II] Title: 복호화, Time: 76 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.java
+++ 백준/Bronze/9046. 복호화/복호화.java
@@ -1,41 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+        
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=0; tc<T; tc++) {
 			String str = br.readLine();
 			
-			Map<Character, Integer> map = new HashMap<>();
-			
+			int[] count = new int[26];
 			for(char c : str.toCharArray()) {
 				if(c == ' ') continue;
 				
-				map.put(c, map.getOrDefault(c, 0)+1);
+				count[c-'a']++;
 			}
 			
-			int max = Integer.MIN_VALUE;
-			for(int n : map.values()) {
-				max = Math.max(max, n);
-			}
+			int max = 0, idx = -1;
+			boolean dup = false;
 			
-			char result = '?';
-			int count = 0;
-			for(char c : map.keySet()) {
-				if(map.get(c) == max) {
-					result = c;
-					count++;
+			for(int i=0; i<26; i++) {
+				if(count[i] > max) {
+					max = count[i];
+					idx = i;
+					dup = false;
+				} else if(count[i]==max && count[i]!=0) {
+					dup = true;
 				}
 			}
 			
-			System.out.println(count>1 ? '?' : result);
+			System.out.println(dup ? '?' : (char)(idx+'a'));
 		}
 	}
 }
