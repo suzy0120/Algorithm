88e07ae5efa75cfbfec42df072a6cf84b0e8730b
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-05-30 14:55:05 +0900

    [Silver IV] Title: 스택, Time: 296 ms, Memory: 17052 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,32 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-        
+		
 		Stack<Integer> stack = new Stack<>();
 		
 		for(int i=0; i<N; i++) {
-			String s = br.readLine();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
 			
-			if(s.contains("push")) {
-				s = s.substring(5);
-				int num = Integer.parseInt(s);
-				stack.push(num);
-			} else if(s.contains("pop")) {
+			if(s.equals("push")) {
+				int n = Integer.parseInt(st.nextToken());
+				stack.push(n);
+			} else if(s.equals("pop")) {
 				if(stack.isEmpty()) System.out.println(-1);
 				else System.out.println(stack.pop());
-			} else if(s.contains("size")) {
-                System.out.println(stack.size());
-            } else if(s.contains("empty")) {
+			} else if(s.equals("size")) {
+				System.out.println(stack.size());
+			} else if(s.equals("empty")) {
 				if(stack.isEmpty()) System.out.println(1);
 				else System.out.println(0);
-			} else if(s.contains("top")) {
+			} else if(s.equals("top")) {
 				if(stack.isEmpty()) System.out.println(-1);
-			    else System.out.println(stack.peek());
+				else System.out.println(stack.peek());
 			}
 		}
 	}
