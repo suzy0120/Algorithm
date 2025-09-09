0dddbcdb389aed9b6bd6c5d0c5ef02f6f79755b3
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2025-09-09 14:26:18 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] price = new int[14];
+        for(int i=0; i<14; i++) {
+        	price[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int remainJ = N;
+        int haveJ = 0;
+        for(int i=0; i<14; i++) {
+			if(remainJ >= price[i]) {
+				haveJ += remainJ / price[i];
+				remainJ %= price[i]; 
+			}
+		}
+        int totalJ = remainJ + haveJ * price[13];
+        
+        int remainS = N;
+        int haveS = 0;
+        int up = 0, down = 0;
+        for(int i=1; i<14; i++) {
+        	if(price[i] > price[i-1]) {
+        		up++;
+        		down = 0;
+        	} else if(price[i] < price[i-1]) {
+        		down++;
+        		up = 0;
+        	}
+        	
+        	if(up >= 3) {
+        		remainS += haveS * price[i];
+        		haveS = 0;
+        	} else if(down >= 3) {
+        		haveS += remainS / price[i];
+        		remainS %= price[i];
+        	}
+		}
+		int totalS = remainS + haveS * price[13];
+        
+		if(totalJ > totalS) System.out.println("BNP");
+        else if(totalJ < totalS) System.out.println("TIMING");
+        else System.out.println("SAMESAME");
+	}
+}
