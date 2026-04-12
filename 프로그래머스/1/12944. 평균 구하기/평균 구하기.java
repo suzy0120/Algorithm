238dcbe0157dbaf0ccf527154acccfaeeff0f9fc
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2026-04-12 21:21:54 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,8 +2,10 @@ class Solution {
     public double solution(int[] arr) {
         double answer = 0;
         
-        for(int n : arr) answer += n;
-        answer = answer/arr.length;
+        for(int n : arr) {
+            answer += n;
+        }
+        answer /= arr.length;
         
         return answer;
     }
