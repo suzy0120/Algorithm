b4d0cc1d65ec6b07d69df8743f11b3f66b1eaefc
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2026-04-16 21:41:14 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 16.71 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -2,10 +2,10 @@ class Solution {
     public long solution(int a, int b) {
         long answer = 0;
         
-        int start = Math.min(a, b);
-        int end = Math.max(a, b);
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
         
-        for(int i=start; i<=end; i++) {
+        for(int i=min; i<=max; i++) {
             answer += i;
         }
         
