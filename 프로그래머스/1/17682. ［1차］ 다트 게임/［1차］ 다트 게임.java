0d4a49892b2037db74b1c9ea6c964298576ad190
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2026-04-20 18:38:11 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.53 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,37 +1,49 @@
-import java.util.*;
-
 class Solution {
     public int solution(String dartResult) {
-        int[] scores = new int[3];
-        String[] s = dartResult.split("(?<=\\D)(?=\\d)");
+        int answer = 0;
         
-        for(int i=0; i<s.length; i++) {
-            int index = 0;
-            while(index < s[i].length() && Character.isDigit(s[i].charAt(index))) {
-                index++;
-            }
-            int score = Integer.parseInt(s[i].substring(0, index));
-            
-            char bonus = s[i].charAt(index);
-            if(bonus=='S')      score = (int) Math.pow(score, 1);
-            else if(bonus=='D') score = (int) Math.pow(score, 2);
-            else if(bonus=='T') score = (int) Math.pow(score, 3);
-            
-            if(s[i].length() > index+1) {
-                char option = s[i].charAt(index+1);
-                if(option=='*') {
-                    score*=2;
-                    if(i>0) scores[i-1]*=2;
-                } else if(option=='#') {
-                    score*=-1;
+        int[] nums = new int[3];
+        int idx = -1;
+        for(int i=0; i<dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+
+            if(Character.isDigit(c)) {
+                idx++;
+                if(c == '1' && dartResult.charAt(i + 1) == '0') {
+                    nums[idx] = 10;
+                    i++;
+                } else {
+                    nums[idx] = c - '0'; 
                 }
             }
-            scores[i] = score;
         }
         
-        int answer = 0;
-        for(int n : scores) answer+=n;
+        String[] str = dartResult.split("[0-9]+");
         
+        int[] values = new int[3];
+        for(int i=0; i<3; i++) {
+            if(str[i+1].charAt(0) == 'S') {
+                if(str[i+1].length() == 1) values[i] += nums[i];
+                else if(str[i+1].charAt(1) == '*') {
+                    values[i] += nums[i] * 2;
+                    if(i > 0) values[i-1] *= 2;
+                } else if(str[i+1].charAt(1) == '#') values[i] += nums[i] * -1;
+            } else if(str[i+1].charAt(0) == 'D') {
+                if(str[i+1].length() == 1) values[i] += Math.pow(nums[i], 2);
+                else if(str[i+1].charAt(1) == '*') {
+                    values[i] += Math.pow(nums[i], 2) * 2;
+                    if(i > 0) values[i-1] *= 2;
+                } else if(str[i+1].charAt(1) == '#') values[i] += Math.pow(nums[i], 2) * -1;
+            } else if(str[i+1].charAt(0) == 'T') {
+                if(str[i+1].length() == 1) values[i] += Math.pow(nums[i], 3);
+                else if(str[i+1].charAt(1) == '*') {
+                    values[i] += Math.pow(nums[i], 3) * 2;
+                    if(i > 0) values[i-1] *= 2;
+                } else if(str[i+1].charAt(1) == '#') values[i] += Math.pow(nums[i], 3) * -1;
+            }
+        }
+        
+        for(int n : values) answer += n;
         return answer;
     }
 }
