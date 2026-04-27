833d80626b0f135fbe3020520da7e92a820d6436
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2026-04-27 10:14:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,17 +1,13 @@
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
+        long n = (long) num;
         int answer = 0;
         
-        while(num > 1) {
-            if(num==1) break;
-            num = num%2==0 ? num/2 : num*3+1;
+        while(n > 1 && answer < 500) {
+            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
             answer++;
-            if(answer==500) {
-                answer = -1;
-                break;
-            }
         }
         
-        return answer;
+        return n == 1 ? answer : -1;
     }
 }
