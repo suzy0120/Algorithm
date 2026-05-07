fbe2b440fc05211e866d516c6473b4a814c0197d
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2026-05-07 23:32:33 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,48 +1,67 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        int count = 0;
-        int zero = 0;
         
-        for(int i=0; i<lottos.length; i++) {
-            if(lottos[i] == 0) zero++;
-        }
-        
-        for(int i=0; i<win_nums.length; i++) {
-            for(int j=0; j<lottos.length; j++) {
-                if(win_nums[i] == lottos[j]) {
-                    count++;
+        int max = 0, zero = 0;
+        for(int l : lottos) {
+            if(l == 0) {
+                zero++;
+                continue;
+            }
+            
+            for(int n : win_nums) {
+                if(l == n) {
+                    max++;
                     break;
                 }
             }
         }
         
-        switch(count) {
-            case 0:
-                answer[1]=6; 
-                break;
+        switch(max+zero) {
             case 1:
-                answer[1]=6;
+                answer[0] = 6;
                 break;
             case 2:
-                answer[1]=5;
+                answer[0] = 5;
                 break;
             case 3:
-                answer[1]=4;
+                answer[0] = 4;
                 break;
             case 4:
-                answer[1]=3;
+                answer[0] = 3;
                 break;
             case 5:
-                answer[1]=2;
+                answer[0] = 2;
                 break;
             case 6:
-                answer[1]=1;
+                answer[0] = 1;
                 break;
+            default:
+                answer[0] = 6;
         }
         
-        answer[0] = answer[1] - zero;
-        if(zero == 6) answer[0] = 1;
+        switch(max) {
+            case 1:
+                answer[1] = 6;
+                break;
+            case 2:
+                answer[1] = 5;
+                break;
+            case 3:
+                answer[1] = 4;
+                break;
+            case 4:
+                answer[1] = 3;
+                break;
+            case 5:
+                answer[1] = 2;
+                break;
+            case 6:
+                answer[1] = 1;
+                break;
+            default:
+                answer[1] = 6;
+        }
         
         return answer;
     }
