098a0d4b18dd8d70126b4a98fdcc6ee7225be3fe
suzy0120 <98311720+suzy0120@users.noreply.github.com>
2026-06-17 19:19:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.30 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,23 +3,21 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<completion.length; i++) {
-            int count = 1;
-            if(map.containsKey(completion[i])) map.replace(completion[i], map.get(completion[i])+1);
-            else map.put(completion[i], count);
+        
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0)+1);
         }
         
-        for(int i=0; i<participant.length; i++) {
-            if(map.containsKey(participant[i])) {
-                if(map.get(participant[i]) == 1) map.remove(participant[i]);
-                else map.replace(participant[i], map.get(participant[i])-1);
-                continue;
+        for(String c : completion) {
+            map.put(c, map.get(c)-1);
+        }
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                answer = key;
+                break;
             }
-            
-            answer = participant[i];
-            break;
         }
         
         return answer;
